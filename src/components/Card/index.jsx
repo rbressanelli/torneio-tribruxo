ad71ba989b567417b808c77cb5d3de299c9ce97e
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-12 18:33:53 -0300

    Feat: Card component added

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,61 @@
+import { CardContainer, Container, ImageContainer } from "../../../styles/styles";
+import Image from "next/image";
+import Gryff from "../../assets/gryffindor.png";
+import Huff from "../../assets/hufflepuff.png";
+import Raven from "../../assets/ravenclaw.png";
+import Slyth from "../../assets/slytherin.png";
+
+const Card = ({ characters }) => {
+
+  const myLoader = ({ src }) => {
+    return characters.image;
+  };
+
+  const charactersHouse = {
+    Gryffindor: "gryffindor",
+    Ravenclaw: "ravenclaw",
+    Slytherin: "slytherin",
+    Hufflepuff: "hufflepuff",
+  };
+
+  return (
+    <Container>
+      {characters && (
+        <CardContainer className={charactersHouse[characters.house]}>
+          <h3>{characters.name}</h3>
+          <div>
+            <ImageContainer
+              loader={myLoader}
+              src={characters.image}
+              alt={characters.name}
+              width={180}
+              height={180}
+              objectFit="cover"
+              placeholder=""
+            />
+          </div>
+          <h3>{characters.actor}</h3>
+          <h3>{characters.house}</h3>
+          <div>
+            <Image
+              src={
+                characters.house === "Gryffindor"
+                  ? Gryff
+                  : characters.house === "Ravenclaw"
+                  ? Raven
+                  : characters.house === "Slytherin"
+                  ? Slyth
+                  : Huff
+              }
+              alt={characters.house}
+              width={70}
+              height={70}
+            />
+          </div>
+        </CardContainer>
+      )}
+    </Container>
+  );
+};
+
+export default Card;
