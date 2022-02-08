5ee558b4b171e340defe950c699f1cd5030f2610
Roberto Bressanelli <rbressanelli@gmail.com>
2022-02-08 15:41:13 -0300

    Refactor: rearranged tests e2e

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,40 +1,51 @@
-
 context("Tribruxo", () => {
-    it('Renders the home page', () => {
-        cy.visit('http://localhost:3000')
-        cy.viewport(1440, 900)
+  it("Renders the home page and verify the components", () => {
+    cy.visit("http://localhost:3000");
+    cy.viewport(1440, 900);
 
-        
-        cy.contains('TORNEIO TRIBRUXO')
+    cy.contains("TORNEIO TRIBRUXO");
 
-        cy.get('button').contains('ESCOLHA OS ALUNOS')
+    cy.get("button").contains("ESCOLHA OS ALUNOS");
 
-        cy.get('span').contains('gryffindor')
-        cy.get('span').contains('hufflepuff')
+    cy.get("span").contains("gryffindor");
+    cy.get("span").contains("hufflepuff");
 
-        cy.get('span').contains('ravenclaw')
+    cy.get("span").contains("ravenclaw");
 
-        cy.get('span').contains('slytherin')
+    cy.get("span").contains("slytherin");
+  });
 
-        cy.get('button').should('have.class', 'modalButton').click({multiple: true, force: true})
+  it("Open the modal em closes it", () => {
+    cy.get("button")
+      .should("have.class", "modalButton")
+      .click({ multiple: true, force: true });
 
-        cy.get('div').should('have.class', 'styles__Modal-sc-1tstb6j-9 aiwjb')
-        cy.get('button').contains('X').click()        
+    cy.get("div").should("have.class", "styles__Modal-sc-1tstb6j-9 jXkhuQ");
+    cy.get("button").contains("X").click();
+  });
 
-        
-        cy.intercept("get", "/", {
-            statusCode: 200,
-        });
-        
-        cy.get('button').contains('ESCOLHA OS ALUNOS').click()
-        cy.get('button').should('have.class', 'styles__Button-sc-1tstb6j-2 faohzy').click({multiple: true, force: true})
+  it("Clicks on the button to sort the caracters", () => {
+    cy.intercept("get", "/", {
+      statusCode: 200,
+    });
 
-        cy.get('div').should('have.class', 'styles__MainContainer-sc-1tstb6j-3 kmGhcE')
-        cy.get('div').should('have.class', 'styles__Container-sc-1tstb6j-6 iLMTDy')
+    cy.get("button").contains("ESCOLHA OS ALUNOS").click();
+    cy.get("button")
+      .should("have.class", "styles__Button-sc-1tstb6j-2 faohzy")
+      .click({ multiple: true, force: true });
 
-        cy.get('button').should('have.class', 'styles__Button-sc-1tstb6j-2 faohzy').click()
-        
-        cy.get('a').contains('back to home').click()
+    cy.get("div").should(
+      "have.class",
+      "styles__MainContainer-sc-1tstb6j-3 kTguuT"
+    );
+    cy.get("div").should("have.class", "styles__Container-sc-1tstb6j-6 gDtbDB");
+  });
 
-    })
-})
+  it("Clicks on the back to home link to back to home page", () => {
+    cy.get("button")
+      .should("have.class", "styles__Button-sc-1tstb6j-2 faohzy")
+      .click();
+
+    cy.get("a").contains("back to home").click();
+  });
+});
